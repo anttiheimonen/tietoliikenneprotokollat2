ea9f52e37b4317424af3c2ed6f4eb6169b64fffb
Antti Heimonen <anttiheimonen@hotmail.com>
2021-08-09 16:28:43 +0300

    Reliable transport protocol done

---
+++ udp/testserver/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace testserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 12345;
+            Console.WriteLine("Olen testiserveri");
+
+            EndPoint endPoint = null;
+            VirtualSocket vs = null;
+            ReliabilityLayer rl = null;
+
+            try
+            {
+                IPEndPoint client = new IPEndPoint(IPAddress.Any, port);
+                endPoint = (EndPoint)client;
+
+                vs = new VirtualSocket(AddressFamily.InterNetwork,
+                       SocketType.Dgram,
+                       ProtocolType.Udp);
+
+                vs.Bind(endPoint);
+                vs.PacketDropRate = 0;
+                vs.PacketDelayRate = 0;
+                vs.PacketDelayTimeSeconds = 1;
+                rl = new ReliabilityLayer(vs, endPoint);
+
+                while (true)
+                {
+                    byte[] message = rl.ReceiveFromWithErrors(ref endPoint);
+
+                    if (message.Length > 0)
+                    {
+                        string s = Encoding.ASCII.GetString(message, 0, message.Length);
+                        System.Console.WriteLine(s);
+                    }
+
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                vs.Close();
+            }
+        }
+    }
+}
